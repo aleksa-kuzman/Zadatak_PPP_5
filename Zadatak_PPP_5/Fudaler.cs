649bb0199baff79799ae79a4567986016025f4ea
Aleksa Kuzman <aleksa.kuzman@cramick-it.com>
2019-12-21 14:18:56 +0100

    ADD: Fudbaler, IFudbaler

---
+++ Zadatak_PPP_5/Fudaler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadatak_PPP_5
+{
+    class Fudaler : IFudbaler
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public int BrojGolova { get; set; }
+        public int BrojDresa { get; set; }
+
+        public void DajGol()
+        {
+            BrojGolova++;
+        }
+
+        public float RacunajProsekGolova(int brojUtakamica)
+        {
+            return (float)BrojGolova / brojUtakamica;
+        }
+    }
+}
---
+++ Zadatak_PPP_5/IFudbaler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadatak_PPP_5
+{
+    public interface IFudbaler
+    {
+        public void DajGol();
+        public float RacunajProsekGolova(int brojUtakamica);
+
+
+    }
+}
